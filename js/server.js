310a2a02c6a03f0fdb95215eb1b4778bfbc2fff2
Marwan <151943696+ItsMarwan@users.noreply.github.com>
2025-01-06 15:18:23 +0200

    Update js

---
+++ js/server.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+
+const DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID;
+const DISCORD_CLIENT_SECRET = process.env.DISCORD_CLIENT_SECRET;
+const DISCORD_REDIRECT_URI = process.env.DISCORD_REDIRECT_URI;
+const GUILD_ID = process.env.GUILD_ID;
