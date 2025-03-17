029c6005174ff90acf4d798d555d421242f51d5d
Marwan <151943696+ItsMarwan@users.noreply.github.com>
2025-03-17 13:34:14 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,3 +6,30 @@ function toggleMenuIcon(button) {
         img.src = "assets/menuIcon.svg";
     }
 }
+
+function youtube() {
+    const APIKey = 'AIzaSyDJwSN96hw_-N3P6FZ32cC1CeGQnMd2im4';
+    const Userid = 'UCsDUR3maxEMihNFGBKOmHLA';
+    const subscriberCount= document.getElementById('subscriberCount');
+    const viewCount = document.getElementById('viewCount');
+    const videoCount = document.getElementById('videoCount');
+
+    let getdata = () => {
+        fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${Userid}&key=${APIKey}`)
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            console.log(data);
+            subscriberCount.innerHTML = data["items"][0].statistics.subscriberCount;
+            viewCount.innerHTML = data["items"][0].statistics.viewCount;
+            videoCount.innerHTML = data["items"][0].statistics.videoCount;
+            
+        })
+    }
+    getdata();
+}
+
+window.onload = function() {
+    youtube();
+};
